64bcbce9c0a2c300ae97bb9971d8ac0a3323db88
Fredric Persson <freper7912@skynet.halmstad.se>
2019-11-12 08:03:00 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/",function(req,res){
+    res.send("index route...");
+});
+
+app.get("/login",function(req,res){
+    res.send("route för att visa inloggningsformulär");
+});
+
+app.post("/login",function(req,res){
+
+    /**
+     * 1. hämta data som klienten skickat ( Repetition )
+     * 2. Leta efter användare i databas/fil/minne
+     * 3. Om användare ej finns skicka respons till klient med error
+     * 4. Om användare finns gå vidare med att kolla lösenord
+     * 5. Om löserord ej är korrekt skicka respons till klient med error
+     * 6. Om lösenord är korrekt - Skicka respons/redirect 
+     * 7. Nu när användaren är inloggad måste hen förbli så ett ta
+     *    Detta löser vi med JWT.
+     *    Skapa JWT och lagra i cookie innan din respons/redirect
+     * 8. Skapa middleware för att skydda vissa routes.
+     *    Här skall vi nu använda våra JWT för att hålla en användare inloggad. 
+     * 9. Småfix för att förbättra säkerhet och fixa utloggning. 
+     */
+
+    res.send(`Route för att hantera själva inloggningen.<br>
+    En del av detta kommer att ske i middleware`);
+
+});
+
+// kollar om systemet har en angiven port, annars 3700...
+const port = process.env.PORT || 3700
+app.listen(port, function(){console.log("port:" +port)});
